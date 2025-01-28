b695c0c62eff4ebaea017b3609b495c65218ba3a
Kaua <etermentales@gmail.com>
2025-01-28 17:04:20 -0300

    login and signup pages

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,9 @@
 <script setup>
-import { RouterView } from 'vue-router';
-
-
 </script>
 
 <template>
-  <RouterView />
+    <h2>Home Page</h2>
 </template>
 
 <style scoped>
-
 </style>
--- src/pages/MyImages.vue
+++ src/pages/MyImages.vue
@@ -1,13 +1,9 @@
 <script setup>
-import { RouterView } from 'vue-router';
-
-
 </script>
 
 <template>
-  <RouterView />
+    <h2>My Images Page</h2>
 </template>
 
 <style scoped>
-
 </style>
