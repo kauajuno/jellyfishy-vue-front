4847e923123352100223205461a08805f4b8eb31
Kaua <etermentales@gmail.com>
2025-01-28 22:22:30 -0300

    Images page layout

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -1,8 +1,6 @@
-
-
 <script setup>
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 
 const user = {
     name: 'Tom Cook',
@@ -11,17 +9,14 @@ const user = {
         'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
-    { name: 'Reports', href: '#', current: false },
+    { name: 'Upload', to: {name: 'Home'}},
+    { name: 'My images', to: {name: 'MyImages'}},
 ]
-const userNavigation = [
-    { name: 'Your Profile', href: '#' },
-    { name: 'Settings', href: '#' },
-    { name: 'Sign out', href: '#' },
-]
+
+function logout() {
+    console.log('logout')
+    // logout logic
+}
 </script>
 
 <template>
@@ -31,35 +26,26 @@ const userNavigation = [
                 <div class="flex h-16 items-center justify-between">
                     <div class="flex items-center">
                         <div class="shrink-0">
-                            <img class="size-8"
+                            <img class="h-8 w-auto"
                                 src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
                                 alt="Your Company" />
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <a v-for="item in navigation" :key="item.name" :href="item.href"
-                                    :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
-                                    :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                                <RouterLink v-for="item in navigation" :key="item.name" :to="item.to"
+                                    :class="[$route.name === item.to.name ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
+                                    :aria-current="$route.name === item.to.name ? 'page' : undefined">{{ item.name }}</RouterLink>
                             </div>
                         </div>
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-4 flex items-center md:ml-6">
-                            <button type="button"
-                                class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                                <span class="absolute -inset-1.5" />
-                                <span class="sr-only">View notifications</span>
-                                <BellIcon class="size-6" aria-hidden="true" />
-                            </button>
-
-                            <!-- Profile dropdown -->
                             <Menu as="div" class="relative ml-3">
                                 <div>
                                     <MenuButton
-                                        class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                                        <span class="absolute -inset-1.5" />
+                                        class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-none">
                                         <span class="sr-only">Open user menu</span>
-                                        <img class="size-8 rounded-full" :src="user.imageUrl" alt="" />
+                                        <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
                                     </MenuButton>
                                 </div>
                                 <transition enter-active-class="transition ease-out duration-100"
@@ -69,11 +55,12 @@ const userNavigation = [
                                     leave-from-class="transform opacity-100 scale-100"
                                     leave-to-class="transform opacity-0 scale-95">
                                     <MenuItems
-                                        class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 focus:outline-hidden">
-                                        <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                                        <a :href="item.href"
-                                            :class="[active ? 'bg-gray-100 outline-hidden' : '', 'block px-4 py-2 text-sm text-gray-700']">{{
-                                            item.name }}</a>
+                                        class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                        <MenuItem v-slot="{ active }">
+                                            <button @click="logout"
+                                                class="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100">
+                                                Sign Out
+                                            </button>
                                         </MenuItem>
                                     </MenuItems>
                                 </transition>
@@ -81,13 +68,11 @@ const userNavigation = [
                         </div>
                     </div>
                     <div class="-mr-2 flex md:hidden">
-                        <!-- Mobile menu button -->
                         <DisclosureButton
-                            class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                            <span class="absolute -inset-0.5" />
+                            class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-none">
                             <span class="sr-only">Open main menu</span>
-                            <Bars3Icon v-if="!open" class="block size-6" aria-hidden="true" />
-                            <XMarkIcon v-else class="block size-6" aria-hidden="true" />
+                            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+                            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
                         </DisclosureButton>
                     </div>
                 </div>
@@ -95,45 +80,33 @@ const userNavigation = [
 
             <DisclosurePanel class="md:hidden">
                 <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-                    <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
-                        :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
-                        :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+                    <RouterLink v-for="item in navigation" :key="item.name" :to="item.to"
+                        :class="[$route.name === item.to.name ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
+                        :aria-current="$route.name === item.to.name ? 'page' : undefined">{{ item.name }}</RouterLink>
                 </div>
                 <div class="border-t border-gray-700 pt-4 pb-3">
                     <div class="flex items-center px-5">
                         <div class="shrink-0">
-                            <img class="size-10 rounded-full" :src="user.imageUrl" alt="" />
+                            <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
                         </div>
                         <div class="ml-3">
-                            <div class="text-base/5 font-medium text-white">{{ user.name }}</div>
+                            <div class="text-base font-medium text-white">{{ user.name }}</div>
                             <div class="text-sm font-medium text-gray-400">{{ user.email }}</div>
                         </div>
-                        <button type="button"
-                            class="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                            <span class="absolute -inset-1.5" />
-                            <span class="sr-only">View notifications</span>
-                            <BellIcon class="size-6" aria-hidden="true" />
-                        </button>
                     </div>
                     <div class="mt-3 space-y-1 px-2">
-                        <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href"
+                        <DisclosureButton @click="logout"
                             class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">
-                            {{ item.name }}</DisclosureButton>
+                            Sign Out
+                        </DisclosureButton>
                     </div>
                 </div>
             </DisclosurePanel>
         </Disclosure>
 
-        <header class="bg-white shadow-sm">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-            </div>
-        </header>
-        <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <!-- Your content -->
-            </div>
-        </main>
+        <RouterView />
+
+       
     </div>
 </template>
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,41 @@
 <script setup>
+import { PhotoIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
-    <h2>Home Page</h2>
+    <header class="bg-white shadow-sm">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Upload</h1>
+        </div>
+    </header>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <div class="mb-4">
+                <label for="cover-photo" class="block text-lg/4 font-medium text-gray-900">Image</label>
+                <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                    <div class="text-center">
+                        <PhotoIcon class="mx-auto size-12 text-gray-300" aria-hidden="true" />
+                        <div class="mt-4 flex text-sm/6 text-gray-600">
+                            <label for="file-upload"
+                                class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:outline-hidden hover:text-indigo-500">
+                                <span>Upload a file</span>
+                                <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                            </label>
+                            <p class="pl-1">or drag and drop</p>
+                        </div>
+                        <p class="text-xs/5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-4">
+                <label for="label" class="block text-sm/4 font-medium text-gray-900">Label</label>
+                <div class="mt-2">
+                    <input type="text" name="label" id="label"
+                        class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
